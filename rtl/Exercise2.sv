541ad7bb68a97a6074204d8a3cc429932dd7d71a
Siddharth Kandpal <123426199+siddharthkandpal@users.noreply.github.com>
2023-02-02 15:21:00 -0500

    Update Exercise2.sv

--- rtl/Exercise2.sv
+++ rtl/Exercise2.sv
@@ -12,5 +12,13 @@ module Exercise2 (
     input [15:0] init,
     output logic [15:0] out
 );
+  reg XORD_BITS;
 
+  always @(posedge) begin
+    if (nReset) 
+      out <= init;
+    else
+    XORD_BITS <= ((init[1] ^ init[3] ^ init[4] ^ init[6]) & 1);
+    out <= ((init >> 1)|(XORD_BITS << 15));
+  end
 endmodule
