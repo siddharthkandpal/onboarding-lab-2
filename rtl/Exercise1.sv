58a2f9b5bcbc450d62cd38e8821c0dd14e413672
Siddharth Kandpal <123426199+siddharthkandpal@users.noreply.github.com>
2023-02-02 14:05:25 -0500

    Update Exercise1.sv

--- rtl/Exercise1.sv
+++ rtl/Exercise1.sv
@@ -13,4 +13,13 @@ module Exercise1 (
     output [7:0] out
 );
 
+  always @(*) begin
+    case(op)
+      2'b00: out = ( a + b );
+      2'b01: out = ( a - b );
+      2'b10: out = ( a & b );
+      2'b11: out = (a | b );
+    endcase
+  end     
+  
 endmodule
