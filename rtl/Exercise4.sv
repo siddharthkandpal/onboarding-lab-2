f94913a54918a12e50b10f8bcae131a3cc8d8f37
Siddharth Kandpal <123426199+siddharthkandpal@users.noreply.github.com>
2023-02-02 14:49:09 -0500

    Update Exercise4.sv

--- rtl/Exercise4.sv
+++ rtl/Exercise4.sv
@@ -20,5 +20,25 @@ module Exercise4 (
 
     output logic [7:0] out
 );
-
+  
+  always @(*) begin
+    casez({cs,sel[1:0]}) 
+      {0, 2'bzz} : begin
+        out = 0;
+      end
+      {1, 2'bz0} : begin
+        out = alpha;
+      end
+      {1, 2'bz1} : begin
+        out = beta;
+      end
+      {1, 2'b10} : begin
+        out = gamma;
+      end
+      {1, 2'b11} : begin
+        out = 0;
+      end
+    endcase
+  end
+  
 endmodule
